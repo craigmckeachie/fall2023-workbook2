8d8e6dbc39b738e2d80de79f92c1557b4316043c
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-18 10:15:28 -0400

    update variable names for clarity

--- tip-amount.js
+++ tip-amount.js
@@ -3,10 +3,9 @@ var app = 9;
 var entree = 19;
 var drink = 11;
 var dessert = 6;
-var tipAmount = 20;
-var tipAmountAsPercentage = 20 / 100;
+var tipPercentage = 20;
 
 costOfFood = app + entree + drink + dessert;
-var tipAmount = costOfFood * tipAmountAsPercentage;
+var tipAmount = costOfFood * (tipPercentage / 100);
 
 console.log("The tip on " + costOfFood + " food bill is " + tipAmount + ".");
