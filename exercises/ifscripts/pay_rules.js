79d55f09502f57913687abb62a774fd5d4afad92
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-19 12:01:55 -0400

    add use strict

--- exercises/ifscripts/pay_rules.js
+++ exercises/ifscripts/pay_rules.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var payRate = 25;
 var hoursWorked = 40;
 var grossPay = 0;
@@ -5,11 +7,11 @@ var grossPay = 0;
 if (hoursWorked <= 40) {
   grossPay = payRate * hoursWorked;
 } else {
-  overtimeHoursWorked = hoursWorked - 40;
-  overtimePayRate = payRate * 1.5;
+  var overtimeHoursWorked = hoursWorked - 40;
+  var overtimePayRate = payRate * 1.5;
   regularPay = payRate * 40;
   overtimePay = overtimeHoursWorked * overtimePayRate;
   grossPay = regularPay + overtimePay;
 }
 
-console.log(grossPay);
+console.log(grossPay.toFixed(2));
