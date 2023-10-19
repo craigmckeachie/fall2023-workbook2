cde6e1bbc9d1af9c1a20ea29d638e789194d63a0
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-19 12:24:06 -0400

    more variables

--- exercises/ifscripts/pay_rules.js
+++ exercises/ifscripts/pay_rules.js
@@ -4,12 +4,13 @@ var payRate = 25;
 var hoursWorked = 40;
 var grossPay = 0;
 var standardHours = 40;
+var overtimePayRateMultiplier = 1.5;
 
 if (hoursWorked <= standardHours) {
   grossPay = payRate * hoursWorked;
 } else {
   var overtimeHoursWorked = hoursWorked - standardHours;
-  var overtimePayRate = payRate * 1.5;
+  var overtimePayRate = payRate * overtimePayRateMultiplier;
   var standardPay = payRate * standardHours;
   overtimePay = overtimeHoursWorked * overtimePayRate;
   grossPay = standardPay + overtimePay;
