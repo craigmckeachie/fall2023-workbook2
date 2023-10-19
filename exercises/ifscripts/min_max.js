2f083b74db3e784f856e0f90e073b294bc5092e3
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-19 13:45:03 -0400

    initial solution

---
+++ exercises/ifscripts/min_max.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+if (a < b && a < c) {
+  console.log("min a: " + a);
+} else if (b < a && b < c) {
+  console.log("min b: " + b);
+} else {
+  console.log("min c: " + c);
+}
+
+if (a > b && a > c) {
+  console.log("max a: " + a);
+} else if (b > a && b > c) {
+  console.log("max b: " + b);
+} else {
+  console.log("max c: " + c);
+}
---
+++ exercises/ifscripts/show_major.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var studentName = "Craig";
+var studentMajorCode = "ENG";
+
+console.log("Student: " + studentName);
+
+if (studentMajorCode == "BIOL") {
+  console.log("Major: Biology");
+  console.log("Advising Location: Science Building, Room 310");
+} else if (studentMajorCode == "CSCI") {
+  console.log("Major: Computer Science");
+  console.log("Advising Location: Shepard Hall, Room 314");
+} else if (studentMajorCode == "ENG") {
+  console.log("Major: English");
+  console.log("Advising Location: Kerr Hall, Room 201");
+}
