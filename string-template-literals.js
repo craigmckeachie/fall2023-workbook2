959fd35dd68bc259cf656a40877fd00db91f818e
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-20 10:05:12 -0400

    template literal example

--- string-template-literals.js
+++ string-template-literals.js
@@ -1,3 +1,5 @@
 let orderNum = 10254;
 let orderStatus = "processing";
 let message = "Order " + orderNum + "'s status is: " + orderStatus;
+let message1 = `Order ${orderNum}'s status is ${orderStatus}   `
+console.log(message1);
