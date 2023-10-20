7aee3db7ca9962fad74c6be51b4fe05d2f94d97b
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-20 10:43:57 -0400

    updates

--- string-template-literals.js
+++ string-template-literals.js
@@ -1,5 +1,9 @@
 let orderNum = 10254;
 let orderStatus = "processing";
+let message2 = "Order " + orderNum;
 let message = "Order " + orderNum + "'s status is: " + orderStatus;
-let message1 = `Order ${orderNum}'s status is ${orderStatus}   `
+let message1 = `
+Order ${orderNum}'s 
+status is ${orderStatus}.
+`
 console.log(message1);
--- syntax/scripts/main.js
+++ syntax/scripts/main.js
@@ -2,5 +2,10 @@ function greet(){
     let dog = "Leo"
     let heading = document.getElementById("title")
     console.log(heading);
+
+    let myInput = document.getElementById("myInput");
+    let currentValue = myInput.value;
+    console.log(currentValue);
+    myInput.value = "Ronald"
 }
 greet();
